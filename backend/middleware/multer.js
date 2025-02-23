d39c94efd2ed68ecf7b9e0ba9a999752ad75ef93
Tisha-Coding <tishacoder@gmail.com>
2025-02-23 21:37:19 +0530

    Backend Code

---
+++ backend/middleware/multer.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    filename:function(req,file,callback)
+    {
+        callback(null,file.originalname);
+    }
+})
+
+const upload = multer({storage})
+
+// have to keep the name storage otherwise this middleware will not work
+
+export default upload;
---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: 
+    {
+        type: String,
+        required: true
+    },
+    email: 
+    {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: 
+    {
+        type: String,
+        required: true
+    },
+    // whenever the user will be created , their cart will be one empty object
+    cartData: 
+    {
+        type: Object,
+        default: {}
+    }
+},{minimize:false}) // mongodb ignores the empty object so that uses this
+
+const userModel = mongoose.models.user || mongoose.model('user',userSchema);
+
+export default userModel;
